a982647dd9ad93d4a8e4cfd276c9332c3f61f1e7
Joey <bernardinij@outlook.com>
2024-09-29 16:21:12 -0500

    9/29

--- rinkDepthClient/rinkDepthClient.py
+++ rinkDepthClient/rinkDepthClient.py
@@ -1,7 +1,27 @@
 import http.client
+from socket import *
 from urllib import response
 
-connection = http.client.HTTPConnection('192.168.2.24', 8080, timeout=10)
-connection.request("GET", "/")
-response = connection.getresponse()
-print("Status: {} and reason: {}".format(response.status, response.reason))
+#connection = http.client.HTTPSConnection('0.0.0.0', 8080, timeout=1000)
+#connection.request("GET", "/")
+#connection.sock.settimeout(1000.0)
+#connection.connect()
+#response = connection.getresponse()
+#print("Status: {} and reason: {}".format(response.status, response.reason))
+#print(connection)
+
+HOST = ''
+PORT = 8080
+with socket(AF_INET, SOCK_STREAM) as sock:
+	sock.bind((HOST, PORT))
+	sock.listen(1)
+	conn, addr = sock.accept()
+	with conn:
+		print('Connected by: ', addr)
+		while True:
+			data = conn.recv(1024)
+			print(data.decode())
+			print("\n\n"+data)
+			if not data: break
+			conn.sendall(data)
+conn.close()
