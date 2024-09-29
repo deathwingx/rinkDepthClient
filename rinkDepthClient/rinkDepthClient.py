b8b9ef45d62400cff9c7fc292b9a6bd48f91daf7
deathwingx <bernardinij@outlook.com>
2024-09-28 20:04:40 -0500

    9/28

--- rinkDepthClient/rinkDepthClient.py
+++ rinkDepthClient/rinkDepthClient.py
@@ -1,1 +1,7 @@
+import http.client
+from urllib import response
 
+connection = http.client.HTTPConnection('192.168.2.24', 8080, timeout=10)
+connection.request("GET", "/")
+response = connection.getresponse()
+print("Status: {} and reason: {}".format(response.status, response.reason))
